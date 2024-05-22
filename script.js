083d86bc1c16106c1055a695f1e06123d861c79c
梶塚太智 <122075081+KajizukaTaichi@users.noreply.github.com>
2024-05-22 15:54:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+let code = document.getElementById("code");
+let cmd = document.getElementById("cmd");
+let disp = document.getElementById("disp");
+
+function print(text) {
+    disp.textContent = `Editan - ${text}`
+}
+
+function insertAtCursor(textarea, textToInsert) {
+    var cursorPosition = textarea.selectionStart;
+    var textBefore = textarea.value.substring(0, cursorPosition);
+    var textAfter = textarea.value.substring(cursorPosition);
+
+    textarea.value = textBefore + textToInsert + textAfter;
+    textarea.selectionStart = textarea.selectionEnd = cursorPosition + textToInsert.length;
+}
+
+function findAndFocus(searchText) {
+    if (searchText === null || searchText === "") {
+        return;
+    }
+
+    let textarea = code;
+
+    var index = code.value.indexOf(searchText);
+    if (index !== -1) {
+        code.focus();
+        textarea.setSelectionRange(index, index + searchText.length);
+        print(`Found ${searchText}`);
+    } else {
+        print("Not found");
+    }
+
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Control") {
+        cmd.focus();
+    } else if (event.code === "Tab" && document.activeElement === code) {
+        event.preventDefault();
+        insertAtCursor(code, '    ');
+    } else if (event.code === "Enter" && document.activeElement === cmd) {
+        event.preventDefault();
+        let order = cmd.value.split(" ")[0];
+        let args = cmd.value.split(" ").slice(1);
+
+        if (order === "save") {
+            localStorage.setItem("code", code.value);
+            print("Saved!");
+        } else if (order === "load") {
+            code.value = localStorage.getItem("code");
+            print("Loaded!");
+        } else if (order === "find") {
+            findAndFocus(args[0]);
+        }
+        cmd.value = "";
+        code.focus();
+    }
+});
